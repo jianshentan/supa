970d498db1d96d61ee578d7b3b7841f9c53591c6
jianshentan <jianshentan@gmail.com>
2016-07-27 06:16:31 +0000

    first commit

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  if (req.mobile == true) {
+    res.render('index', { mobile: "true" });
+  } else {
+    res.render('index', { mobile: "false" });
+  }
+});
+
+module.exports = router;
